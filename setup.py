9e58d7f21c4cbd17b38b5a8f60281c70ad9214a4
ssarusi <sarusi.shani@intel.com>
2019-08-20 14:50:52 +0300

    third commit (testing)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Machine-learning based minimal MLST scheme for bacterial strain typing",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/shanicohen33/minMLST",
     packages=setuptools.find_packages(),
     # install_requires=[
     #     'markdown',
