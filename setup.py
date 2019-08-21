80d8b5b406dcf5156335f5979500ffd41a6577ae
ssarusi <sarusi.shani@intel.com>
2019-08-21 16:37:39 +0300

    v6

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="minmlst-v5",
+setup(
+    name="minmlst-v6",
     version="0.0.1",
     author="Shani Cohen",
     author_email="shani.cohen.33@gmail.com",
@@ -12,14 +12,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shanicohen33/minMLST",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     install_requires=[
-        'shap',
-        # 'xgboost>=0.82',
-        'pandas',
-        'numpy',
-        'scikit-learn'
-        # 'pickle'
+        'shap>=0.28.5',
+        'pandas>=0.19.2',
+        'numpy>=1.16.2',
+        'scikit-learn>=0.20.1'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
